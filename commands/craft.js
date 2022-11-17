23d4b241eb1d86985129a1c922860c1cb5f3bd39
Bartosz <bartoszsdr@gmail.com>
2022-11-17 20:05:45 +0100

    fix commands

--- commands/craft.js
+++ commands/craft.js
@@ -22,28 +22,4 @@ module.exports = {
 			)
 		return message.channel.send({ embeds: [recipesEmbed] })
 	},
-	// 	function dropItems() {
-	// 		for (let i = correctIndex.length - 1; i >= 0; i--) {
-	// 			backpack.splice(correctIndex[i], 1)
-	// 		}
-	// 	}
-
-	// 	for (let i = 0; i < correctIndex.length; i++) {
-	// 		if (backpack[correctIndex[i]]) {
-	// 			dropItems()
-	// 			fs.writeFile('./data/players.json', JSON.stringify(players), err => {
-	// 				if (err) console.log(err)
-	// 			})
-	// 			const dropEmbed = new EmbedBuilder()
-	// 				.setColor(0x992e22)
-	// 				.setDescription(`Wyrzucone przedmioty: ${correctIndex.length}.`)
-	// 			return message.channel.send({ embeds: [dropEmbed] })
-	// 		} else {
-	// 			const dropErrorEmbed = new EmbedBuilder()
-	// 				.setColor(0x992e22)
-	// 				.setDescription('Podano pusty slot.')
-	// 			return message.channel.send({ embeds: [dropErrorEmbed] })
-	// 		}
-	// 	}
-	// },
 }
